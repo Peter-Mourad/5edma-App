dbeb5dd5c67b508c018c514ef5ae36f0efaaa8f2
peter-mourad <pmourad772@gmail.com>
2024-09-18 13:14:06 +0300

    Added the new routes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const signUp = require('./routes/signUp');
 const login = require('./routes/login');
+const logout = require('./routes/logout');
+const profile = require('./routes/profile');
 
 const app = express();
 
+app.use(express.json());
 app.use(signUp);
 app.use(login);
+app.use(logout);
+app.use(profile);
 
 const port = process.env.PORT;
 app.listen(port, () => {
