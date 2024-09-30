398d8eba3df38970913c31b2ca77aee09eaae061
peter-mourad <pmourad772@gmail.com>
2024-09-30 13:43:25 +0300

    added classuser and relationships

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,14 +1,19 @@
 const sequelize = require('./connection');
 const User = require('./user');
 const Class = require('./class');
+const ClassUser = require('./classUsers');
 
 const db = {
   sequelize,
   User,
-  Class
+  Class,
+  ClassUser
 }
 
-User.hasMany(Class, { foreignKey: 'creatorID' })
-Class.belongsTo(User, { foreignKey: 'creatorID' });
+User.hasMany(Class, { foreignKey: 'creatorId' })
+Class.belongsTo(User, { foreignKey: 'creatorId' });
+
+User.belongsToMany(Class, { through: ClassUser, foreignKey: 'userId', otherKey: 'classId' });
+Class.belongsToMany(User, { through: ClassUser, foreignKey: 'classId', otherKey: 'userId' });
 
 module.exports = db;
