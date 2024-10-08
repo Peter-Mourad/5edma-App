38e5e45a617681abb555ffca11c311b2b6fa7a90
peter-mourad <pmourad772@gmail.com>
2024-10-08 11:50:00 +0300

    Added multer middleware for uploading images

---
+++ backend/middlewares/upload.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+// Ensure the 'uploads' folder exists
+const uploadsDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadsDir);
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
