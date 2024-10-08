d0b841255b4f0a964d0f87f4311c356ac9743526
peter-mourad <pmourad772@gmail.com>
2024-10-08 15:11:47 +0300

    Added needed logic and fixed bugs

--- backend/controllers/userProfileController.js
+++ backend/controllers/userProfileController.js
@@ -1,40 +1,65 @@
 const { User } = require('../models/index');
+const multer = require('multer');
+const { uploadImage } = require('../services/cloudinaryService');
+const { error } = require('console');
 
 const getUserProfile = async (req, res) => {
-    const user = await User.findByPk(req.user.id);
-
-    return res.json({
-        "userId": user.userId,
-        "firstName": user.firstName,
-        "lastName": user.lastName,
-        "email": user.email,
-        "role": user.role,
-        "birthdate": user.birthdate,
-        "qrCode": user.qrCode,
-    });
-};
-
-const deleteUserAccount = async (req, res) => {
     try {
-        const result = await User.destroy({ where: { userId: req.user.id, } });
-
-        if (!result) {
-            res.status(404).json({ message: `User Not found.` });
-        }
-        res.json({ message: `User was deleted successfully.` });
+        const user = await User.findByPk(req.user.id);
+        return res.json({
+            "firstName": user.firstName,
+            "lastName": user.lastName,
+            "email": user.email,
+            "birthdate": user.birthdate,
+            "qrCode": user.qrCode,
+            "profilePicture": user.profilePicture
+        });
     } catch (error) {
-        res.status(500).json({ message: 'Internal Server Error', error: error.message });
+        return res.status(500).json({ error: error.message });
     }
 };
 
 const updateUserData = async (req, res) => {
-    const user = await User.findByPk(req.user.id);
     try {
-        await user.update(req.body.user);
-        return res.send({ user });
+        const user = await User.findByPk(req.user.id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        if (req.body.email) {
+            const emailExists = await User.findOne({ where: { email: req.body.email } });
+            if (emailExists) {
+                return res.status(400).json({ error: 'This email is already in use!' });
+            }
+        }
+
+        const updatedUserData = {
+            firstName: req.body.firstName || user.firstName,
+            lastName: req.body.lastName || user.lastName,
+            email: req.body.email || user.email,
+            birthdate: req.body.birthdate || user.birthdate
+        };
+
+        await user.update(updatedUserData);
+        return res.send(updatedUserData);
     } catch (error) {
-        return res.status(403).send({ error: error });
+        return res.status(500).send({ error: error.message });
     }
 };
 
-module.exports = {getUserProfile, deleteUserAccount, updateUserData}
+const updateUserProfilePicture = async (req, res) => {
+    try {
+        const filePath = req.file.path;
+        if (!filePath) {
+            return res.status(400).json({ error: 'No file is provided!' });
+        }
+
+        const photoUrl = await uploadImage(filePath);
+        await User.update({'profilePicture' : photoUrl}, { where: { userId: req.user.id } });
+        return res.status(200).json({ message: 'Photo uploaded successfully', photoUrl });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { getUserProfile, updateUserData, updateUserProfilePicture };
