f4544e868d50a48c763690cf8f10d6999236fba5
peter-mourad <pmourad772@gmail.com>
2024-10-08 15:06:50 +0300

    made qr code generated from user id

--- backend/utils/qrCodeGenerator.js
+++ backend/utils/qrCodeGenerator.js
@@ -1,12 +1,13 @@
 const QRCode = require('qrcode');
 
-const generateQRCode = async (userID) => {
-  try {
-    const url = await QRCode.toDataURL(userID);
-    return url;
-  } catch (err) {
-    console.error('Error generating QR Code', err);
-  }
+const generateQRCode = async (email) => {
+    try {
+        const url = await QRCode.toDataURL(email);
+        return url;
+    } catch (err) {
+        console.error('Error generating QR Code', err);
+        return null;
+    }
 };
 
 module.exports = generateQRCode;
